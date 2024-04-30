5eb53f0c59760fce1055a4bad2c4a6788baa565f
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-04-29 22:10:23 -0300

    Incrementos JavaScript

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,36 +1,102 @@
 
-function addCard() {
+const allStock = [
+  {
+    bolsa: 'NASDAQ',
+    codigo: 'AAPL',
+    empresa: 'Apple Inc',
+    valor: 18050,
+    variacao: 0.25,
+    nAcoes: 20,
+    setor: 'Electronic Technology',
+    site: 'https://www.apple.com'
+  },
+  {
+    bolsa: 'NASDAQ',
+    codigo: 'MSFT',
+    empresa: 'Microsoft Corp',
+    valor: 21100,
+    variacao: 2.15,
+    nAcoes: 15,
+    setor: 'Technology Services',
+    site: 'https://www.microsoft.com'
+  },
+  {
+    bolsa: 'NASDAQ',
+    codigo: 'NVDA',
+    empresa: 'NVIDIA Corp',
+    valor: 18500,
+    variacao: -1.25,
+    nAcoes: 10,
+    setor: 'Electronic Technology',
+    site: 'https://www.nvidia.com'
+  }
+]
+
+function addCard(stock) {
   const listaCards = document.querySelector('body > main ')
 
   listaCards.innerHTML = listaCards.innerHTML + `
   <div class="card-ticket">
         <header>
-          <h2><span>NASDAQ:</span> AAPL</h2>
-          <h1>Apple Inc</h1>
+          <h2><span>${stock.bolsa}:</span>${stock.codigo}</h2>
+          <h1>${stock.empresa}</h1>
         </header>
 
         <main>
           <div>
             <h2 class="value">Valor:</h2>
-            <span>180.95 BRL</span>
+            <span>${realFormat(parseInt(stock.valor / 100))}</span>
           </div>
 
           <div class="teste">
             <p class="value">Variação:</p>
-            <span class="pos">▲ +0.25%</span>
-            <span>USD$ 0,98</span>
+            <span ${stock.variacao < 0 ? 'class="neg"' : 'class="pos"'}}> ${stock.variacao < 0 ? '▼' : '▲'}  ${stock.variacao} %</span>
+            <span>USD$ ${realFormat((+stock.valor / 100) * (+stock.variacao / 100))}</span>
           </div>
         </main>
 
         <footer>
           <div>
-            <p>20</p>
+            <p>${stock.nAcoes}</p>
             <span>Ações</span>
           </div>
           <div>
-            <p>USD$ 5.170,05</p>
+            <p>USD$  ${realFormat((+stock.nAcoes) * (+stock.valor / 100))}</p>
             <span>Posição</span>
           </div>
         </footer>
       </div>`
+}
+
+function realFormat(valor) {
+  return valor.toFixed(2).toString().replace('.', ',')
+}
+
+function loadCard() {
+  allStock.map((stock) => {
+    addCard(stock)
+  })
+}
+
+function addTable(stock) {
+  const main = document.querySelector('#tb2')
+
+  main.innerHTML = main.innerHTML + `
+ 
+ <tr class="i" >
+ <td>${stock.bolsa}</td>
+ <td>${stock.codigo}</td>
+ <td>${stock.empresa}</td>
+ <td>${realFormat(parseInt(stock.valor / 100))}</td>      
+ <td>USD$ ${realFormat((+stock.valor / 100) * (+stock.variacao / 100))}</td>
+ <td>${stock.nAcoes}</td>
+ <td>USD$ ${realFormat((+stock.nAcoes) * (+stock.valor / 100))}</td>
+ </tr>
+ `
+}
+
+function loadTable() {
+  allStock.map((stock) => {
+    addTable(stock)
+  })
 }
